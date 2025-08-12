97335081f9b892289f6d23abc45b07b861699af6
Ryan Deivert <radeivert@gmail.com>
2025-08-12 15:50:21 -0700

    removed unnecessary files/folders

--- build.zig
+++ build.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const linkage = b.option(std.Build.LinkMode, "linkage", "Type of linkage") orelse .static;
+
+    const lib = b.addLibrary(.{
+        .name = "sdl_mixer",
+        .linkage = linkage,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+
+    lib.addCSourceFiles(.{
+        .root = b.path("src"),
+        .files = &.{},
+    });
+    lib.addIncludePath(b.path("include"));
+
+    b.installArtifact(lib);
 }
